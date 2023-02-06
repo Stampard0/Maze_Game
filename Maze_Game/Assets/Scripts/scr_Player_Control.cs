ec52fa3503178a73a41e09a4c16dc36bfbacf06e
Stampard0 <98725092+Stampard0@users.noreply.github.com>
2023-02-06 16:09:50 +0800

    done something

--- Maze_Game/Assets/Scripts/scr_Player_Control.cs
+++ Maze_Game/Assets/Scripts/scr_Player_Control.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class scr_Player_Control : MonoBehaviour
 {
     private CharacterController CharControl;
-    private Vector3 input;
+    private Vector3 motion;
+    private float currentSpeed = 0;
+    private float velocity = 0;
     [SerializeField] float crouchSpeedMultiplier = 0.5f; //crouch speed
     [SerializeField] float jumpForce = 0.5f; // jump force
-    [SerializeField] int gravity = 1;
-    [SerializeField] int speed = 1;
+    [SerializeField] float gravity = 3.5f; //[Range(1f, 10f)] // gravity
+    [SerializeField] float speed = 2f; //[Range(0.1f,1f)] // normal player speed
     private bool crouching = false; //crouch bool
-    //private bool isGrounded = false; //not on ground
+    private bool isGrounded = false; //not on ground
+
+    // Awake is called before start method
+    private void Awake()
+    {
+        CharControl = GetComponent<CharacterController>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        CharControl = GetComponent<CharacterController>();
+        currentSpeed = speed;
     }
 
     // Update is called once per frame
@@ -64,5 +72,15 @@ public class scr_Player_Control : MonoBehaviour
             CharControl.Move(motion.normalized * speed * Time.deltaTime);
             //transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
+    }
+
+    void ApplyMovement()
+    {
+        float inputY = Input.GetAxisRaw("Horizontal") * currentSpeed;
+        float inputX = Input.GetAxisRaw("Vertical") * currentSpeed;
+        motion += transform.forward * inputX * Time.deltaTime;
+        motion += transform.right * inputY * Time.deltaTime;
+        motion.y += velocity * Time.deltaTime;
+        controller.Move(motion);
     }
 }
